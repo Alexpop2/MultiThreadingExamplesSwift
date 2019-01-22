9b0583d43a6cf2da276328d0631914738f2c56c4
alex7893 <alexqw1@ya.ru>
2019-01-22 22:25:36 +0300

    Initial Commit

---
+++ MultiThreadingExamplesSwiftTests/MultiThreadingExamplesSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  MultiThreadingExamplesSwiftTests.swift
+//  MultiThreadingExamplesSwiftTests
+//
+//  Created by Рабочий on 22/01/2019.
+//  Copyright © 2019 Рабочий. All rights reserved.
+//
+
+import XCTest
+@testable import MultiThreadingExamplesSwift
+
+class MultiThreadingExamplesSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
